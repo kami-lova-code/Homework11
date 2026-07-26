404fd45ac90246a5d6b21b75e15013bb2eacc5f4
Джамиля <rabiyatochka2017@yandex.ru>
2026-07-26 22:26:34 +0300

    Выполнила домашнее задание.

---
+++ Main.java
@@ -0,0 +1,24 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+
+        System.out.println("ФИО сотрудника - " + fullName);
+
+
+        String fullNameOne = "Ivanov Ivan Ivanovich";
+        System.out.println("Данные ФИО сотрудника " + fullNameOne.toUpperCase());
+
+
+        String fullNameTwo = "Иванов Семён Семёнович";
+        fullNameTwo = fullNameTwo.replace("ё", "е");
+
+        System.out.println("Данные ФИО сотрудника — " + fullNameTwo);
+
+    }
+
+    }
